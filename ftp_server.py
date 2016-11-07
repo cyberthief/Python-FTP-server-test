415520e8a1fa599c01ae3aa345adcf01360cde91
cyberthief <revervand@mail.ru>
2016-11-08 00:39:18 +0300

    Update ftp_server.py

--- ftp_server.py
+++ ftp_server.py
@@ -4,14 +4,14 @@ from pyftpdlib.servers import FTPServer
 
 authorizer = DummyAuthorizer()
 
-authorizer.add_user("user", "12345", "/home/aero", perm="elradfmw")
+authorizer.add_user("user", "12345", "/home/kronos", perm="elradfmw")
 authorizer.add_user("backdoor", "xaker1337", "/home/xaker", perm="elradfmw")
 
 
 handler = FTPHandler
 handler.authorizer = authorizer
 
-# Pentest_Host1_Flag4:Aero{d0nt_sh4r3_y0u_h4cks}
+# Pentes_Host1_Flag4:Aero{d0nt_sh4r3_y0u_h4cks}
 
 server = FTPServer(("0.0.0.0", 21), handler)
 server.serve_forever()
