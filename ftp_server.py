93c8ba2db1daeefd1e24d2fe80f88e1c52b63b00
cyberthief <revervand@mail.ru>
2016-11-08 00:34:00 +0300

    Update ftp_server.py

--- ftp_server.py
+++ ftp_server.py
@@ -6,7 +6,7 @@ authorizer = DummyAuthorizer()
 
 authorizer.add_user("user", "12345", "/home/aero", perm="elradfmw")
 authorizer.add_user("backdoor", "xaker1337", "/home/xaker", perm="elradfmw")
-authorizer.add_anonymous("/home/nobody")
+
 
 handler = FTPHandler
 handler.authorizer = authorizer
